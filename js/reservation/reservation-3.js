8ec493b436ca21198fd6d27f3b1ed0ee652840a7
An <Ans>
2025-11-19 23:06:38 +0700

    23540051 - change src link of nav

--- js/reservation/reservation-3.js
+++ js/reservation/reservation-3.js
@@ -137,7 +137,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const navTop = document.getElementById("top-page");
   if (navTop) {
     navTop.addEventListener("click", function () {
-      window.location.href = "../index.html";
+      window.location.href = "../homePage.html";
     });
   }
 });
